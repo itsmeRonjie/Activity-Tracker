32ce305061b8d0ff48e25550a4f6f2976ff5a9cf
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-05-07 14:57:35 +0800

    - added Chart

--- Activity-Tracker/View/Main Views/MainView.swift
+++ Activity-Tracker/View/Main Views/MainView.swift
@@ -12,12 +12,32 @@ import SwiftData
 struct MainView: View {
     @Query(sort: \Activity.name, order: .forward) var activities: [Activity]
     @Environment(\.modelContext) private var context
+    
+    @State private var newName: String = ""
+    @State private var newHoues: Double = 0
+    @State private var currentActivty: Activity? = nil
+    @State private var selectCount: Int?
+    
 
     var body: some View {
         NavigationStack {
             VStack {
-                
+                Chart {
+                    ForEach(activities) { activity in
+                        let isSelected: Bool = activity == currentActivty
+                        SectorMark(
+                            angle: .value("Activities", activity.hoursPerDay),
+                            innerRadius: .ratio(0.6),
+                            outerRadius: .ratio(
+                                isSelected ? 1.05 : 0.95
+                            ),
+                            angularInset: 1
+                        )
+                        .foregroundStyle(.red)
+                    }
+                }
             }
+            .padding()
             .navigationTitle("Activity Tracker")
         }
     }
